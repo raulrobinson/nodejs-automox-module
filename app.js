07865ee66aef509ab8751351c9ada263b78350a9
rasysbox <rasysbox@hotmail.com>
2024-04-01 11:17:48 -0500

    add app_id y cors

--- app.js
+++ app.js
@@ -1,8 +1,32 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
+
+const corsOptions = {
+    origin: "http://localhost:3000"
+};
+
+const app_ids = [
+    process.env.APP_ID_MAIN,
+    process.env.APP_ID_SUB
+];
+
+app.use((req, res, next) => {
+    const appId = req.headers['app_id'];
+    if (app_ids.includes(appId)) {
+        next();
+    } else {
+        res.status(403).send('Access Denied.');
+    }
+});
+
+app.use(cors(corsOptions));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
     res.send({ message: "Hello World" });
